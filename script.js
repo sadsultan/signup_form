e71a6171672e9da2b1a0f47ab2adc23246f0c71b
Saad <66124515+sadsultan@users.noreply.github.com>
2023-07-04 22:06:44 +0500

    Added form validation

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-let form = document.getElementById("card")
+let card = document.getElementById("card")
 let logo=document.getElementById("logo")
 
 setTimeout(()=>{
-    form.classList.remove("animation")
+    card.classList.remove("animation")
     logo.classList.remove("logo2")
 }, 300);
+function validateForm(){
+    let password = document.getElementById("password").value
+    let confirmPassword = document.getElementById("confirmPassword").value
+
+    if (password != confirmPassword) {
+        alert("Password and Confirm Password do not match")
+        return false
+    }
+}
