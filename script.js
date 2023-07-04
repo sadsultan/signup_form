2541072758596838836c3cc0e96b175a471f5774
Saad <66124515+sadsultan@users.noreply.github.com>
2023-07-04 20:13:49 +0500

    Added an animation to submission form

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+let form = document.getElementById("card")
 let logo=document.getElementById("logo")
 
 setTimeout(()=>{
+    form.classList.remove("animation")
     logo.classList.remove("logo2")
 }, 300);
-
